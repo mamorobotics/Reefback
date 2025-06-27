ff4aef235fccc398ea02231121dd8b23b79515b8
Miles Contopoulos <107159084+TheMagnificentMiles@users.noreply.github.com>
2025-06-27 10:39:32 -0400

    UI technically works

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
 use crate::{network_interface::*, network_manager::*};
+use imgui::*;
 
 mod network_interface;
 mod network_manager;
+mod ui_manager;
 
 fn main() {
+    let mut value = 0;
+    let choices = ["test test this is 1", "test test this is 2"];
+    ui_manager::simple_init(file!(), move |_, ui| {
+        ui.window("Hello world")
+            .size([300.0, 110.0], Condition::FirstUseEver)
+            .build(|| {
+                ui.text_wrapped("Hello world!");
+                ui.text_wrapped("こんにちは世界！");
+                if ui.button(choices[value]) {
+                    value += 1;
+                    value %= 2;
+                }
+
+                ui.button("This...is...imgui-rs!");
+                ui.separator();
+                let mouse_pos = ui.io().mouse_pos;
+                ui.text(format!(
+                    "Mouse Position: ({:.1},{:.1})",
+                    mouse_pos[0], mouse_pos[1]
+                ));
+            });
+    });
+
     //Create a connection type
     let mut connection: Connection<SimNetworkInterface> = Connection {
         interface: SimNetworkInterface::new(),
