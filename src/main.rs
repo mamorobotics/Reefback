8481a0fb27eb391d6935bfbb48e5f7d1498472fc
Miles Contopoulos <107159084+TheMagnificentMiles@users.noreply.github.com>
2025-08-20 09:36:28 -0400

    Working on UI, possibly finished

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,13 @@ mod network_manager;
 mod ui_manager;
 
 fn main() {
-    let mut value = 0;
-    let choices = ["test test this is 1", "test test this is 2"];
-    ui_manager::simple_init(file!(), move |_, ui| {
-        ui.window("Hello world")
-            .size([300.0, 110.0], Condition::FirstUseEver)
-            .build(|| {
-                ui.text_wrapped("Hello world!");
-                ui.text_wrapped("こんにちは世界！");
-                if ui.button(choices[value]) {
-                    value += 1;
-                    value %= 2;
-                }
-
-                ui.button("This...is...imgui-rs!");
-                ui.separator();
-                let mouse_pos = ui.io().mouse_pos;
-                ui.text(format!(
-                    "Mouse Position: ({:.1},{:.1})",
-                    mouse_pos[0], mouse_pos[1]
-                ));
-            });
+    //Initialize the UI
+    ui_manager::create_ui(|ui: &mut Ui| {
+        ui.window("Hello").size([300.0, 100.0], imgui::Condition::FirstUseEver).build(
+            || {
+                ui.text("Hello!")
+            }
+        );
     });
 
     //Create a connection type
--- src/ui_manager.rs
+++ src/ui_manager.rs
@@ -1,111 +1,170 @@
-use glium::glutin::surface::WindowSurface;
-use glium::{Display, Surface};
-use imgui::{Context, Ui};
-use imgui_glium_renderer::Renderer;
-use imgui_winit_support::winit::dpi::LogicalSize;
-use imgui_winit_support::winit::event::{Event, WindowEvent};
-use imgui_winit_support::winit::event_loop::EventLoop;
-use imgui_winit_support::winit::window::WindowAttributes;
-use imgui_winit_support::{HiDpiMode, WinitPlatform};
-use std::path::Path;
-use std::time::Instant;
+use std::{num::NonZeroU32, thread, time::Instant};
 
-#[allow(dead_code)] // annoyingly, RA yells that this is unusued
-pub fn simple_init<F: FnMut(&mut bool, &mut Ui) + 'static>(title: &str, run_ui: F) {
-    init_with_startup(title, |_, _, _| {}, run_ui);
-}
+use glow::HasContext;
+use glutin::{
+    config::ConfigTemplateBuilder,
+    context::{ContextAttributesBuilder, NotCurrentGlContext, PossiblyCurrentContext},
+    display::{GetGlDisplay, GlDisplay},
+    surface::{GlSurface, Surface, SurfaceAttributesBuilder, WindowSurface},
+};
 
-pub fn init_with_startup<FInit, FUi>(title: &str, mut startup: FInit, mut run_ui: FUi)
-where
-    FInit: FnMut(&mut Context, &mut Renderer, &Display<WindowSurface>) + 'static,
-    FUi: FnMut(&mut bool, &mut Ui) + 'static,
-{
-    let mut imgui = Context::create();
+use imgui_winit_support::{
+    winit::{
+        dpi::LogicalSize,
+        event_loop::EventLoop,
+        window::{Window, WindowAttributes},
+    },
+    WinitPlatform,
+};
+use raw_window_handle::HasWindowHandle;
 
-    let title = match Path::new(&title).file_name() {
-        Some(file_name) => file_name.to_str().unwrap(),
-        None => title,
-    };
-    let event_loop = EventLoop::new().expect("Failed to create EventLoop");
+pub fn create_ui(draw_ui: fn(&mut imgui::Ui)) {
+    let (event_loop, window, surface, context) = create_window();
+    let (mut winit_platform, mut imgui_context) = imgui_init(&window);
 
-    let window_attributes = WindowAttributes::default()
-        .with_title(title)
-        .with_inner_size(LogicalSize::new(1024, 768));
-    let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new()
-        .set_window_builder(window_attributes)
-        .build(&event_loop);
-    let mut renderer = Renderer::new(&mut imgui, &display).expect("Failed to initialize renderer");
+    let gl = glow_context(&context);
 
-    let mut platform = WinitPlatform::new(&mut imgui);
-    {
-        let dpi_mode = if let Ok(factor) = std::env::var("IMGUI_EXAMPLE_FORCE_DPI_FACTOR") {
-            // Allow forcing of HiDPI factor for debugging purposes
-            match factor.parse::<f64>() {
-                Ok(f) => HiDpiMode::Locked(f),
-                Err(e) => panic!("Invalid scaling factor: {}", e),
-            }
-        } else {
-            HiDpiMode::Default
-        };
-
-        platform.attach_window(imgui.io_mut(), &window, dpi_mode);
-    }
+    let mut ig_renderer = imgui_glow_renderer::AutoRenderer::new(gl, &mut imgui_context)
+        .expect("failed to create renderer");
 
     let mut last_frame = Instant::now();
 
-    startup(&mut imgui, &mut renderer, &display);
-
     #[allow(deprecated)]
     event_loop
-        .run(move |event, window_target| match event {
-            Event::NewEvents(_) => {
-                let now = Instant::now();
-                imgui.io_mut().update_delta_time(now - last_frame);
-                last_frame = now;
-            }
-            Event::AboutToWait => {
-                platform
-                    .prepare_frame(imgui.io_mut(), &window)
-                    .expect("Failed to prepare frame");
-                window.request_redraw();
-            }
-            Event::WindowEvent {
-                event: WindowEvent::RedrawRequested,
-                ..
-            } => {
-                let ui = imgui.frame();
+        .run(move |event, window_target| {
+            match event {
+                winit::event::Event::NewEvents(_) => {
+                    let now = Instant::now();
+                    imgui_context
+                        .io_mut()
+                        .update_delta_time(now.duration_since(last_frame));
+                    last_frame = now;
+                }
+                winit::event::Event::AboutToWait => {
+                    winit_platform
+                        .prepare_frame(imgui_context.io_mut(), &window)
+                        .unwrap();
+                    window.request_redraw();
+                }
+                winit::event::Event::WindowEvent {
+                    event: winit::event::WindowEvent::RedrawRequested,
+                    ..
+                } => {
+                    unsafe { ig_renderer.gl_context().clear(glow::COLOR_BUFFER_BIT) };
 
-                let mut run = true;
-                run_ui(&mut run, ui);
-                if !run {
+                    let ui = imgui_context.new_frame();
+                    ui.dockspace_over_main_viewport();
+
+                    draw_ui(ui);
+                    
+                    winit_platform.prepare_render(ui, &window);
+                    let draw_data = imgui_context.render();
+
+                    ig_renderer.render(draw_data).expect("error rendering imgui");
+                    surface.swap_buffers(&context).expect("Failed to swap buffers");
+                }
+                winit::event::Event::WindowEvent {
+                    event: winit::event::WindowEvent::CloseRequested,
+                    ..
+                } => {
                     window_target.exit();
                 }
-
-                let mut target = display.draw();
-                target.clear_color_srgb(1.0, 1.0, 1.0, 1.0);
-                platform.prepare_render(ui, &window);
-                let draw_data = imgui.render();
-                renderer
-                    .render(&mut target, draw_data)
-                    .expect("Rendering failed");
-                target.finish().expect("Failed to swap buffers");
-            }
-            Event::WindowEvent {
-                event: WindowEvent::Resized(new_size),
-                ..
-            } => {
-                if new_size.width > 0 && new_size.height > 0 {
-                    display.resize((new_size.width, new_size.height));
+                winit::event::Event::WindowEvent {
+                    event: winit::event::WindowEvent::Resized(new_size),
+                    ..
+                } => {
+                    if new_size.width > 0 && new_size.height > 0 {
+                        surface.resize(
+                            &context,
+                            NonZeroU32::new(new_size.width).unwrap(),
+                            NonZeroU32::new(new_size.height).unwrap(),
+                        );
+                    }
+                    winit_platform.handle_event(imgui_context.io_mut(), &window, &event);
                 }
-                platform.handle_event(imgui.io_mut(), &window, &event);
-            }
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => window_target.exit(),
-            event => {
-                platform.handle_event(imgui.io_mut(), &window, &event);
+                event => {
+                    winit_platform.handle_event(imgui_context.io_mut(), &window, &event);
+                }
             }
         })
         .expect("EventLoop error");
+}
+
+fn create_window() -> (
+    EventLoop<()>,
+    Window,
+    Surface<WindowSurface>,
+    PossiblyCurrentContext,
+) {
+    let event_loop = EventLoop::new().unwrap();
+
+    let window_attributes = WindowAttributes::default()
+        .with_title("Leviathan")
+        .with_inner_size(LogicalSize::new(1024, 768));
+    let (window, cfg) = glutin_winit::DisplayBuilder::new()
+        .with_window_attributes(Some(window_attributes))
+        .build(&event_loop, ConfigTemplateBuilder::new(), |mut configs| {
+            configs.next().unwrap()
+        })
+        .expect("Failed to create OpenGL window");
+
+    let window = window.unwrap();
+
+    let context_attribs =
+        ContextAttributesBuilder::new().build(Some(window.window_handle().unwrap().as_raw()));
+    let context = unsafe {
+        cfg.display()
+            .create_context(&cfg, &context_attribs)
+            .expect("Failed to create OpenGL context")
+    };
+
+    let surface_attribs = SurfaceAttributesBuilder::<WindowSurface>::new()
+        .with_srgb(Some(true))
+        .build(
+            window.window_handle().unwrap().as_raw(),
+            NonZeroU32::new(1024).unwrap(),
+            NonZeroU32::new(768).unwrap(),
+        );
+    let surface = unsafe {
+        cfg.display()
+            .create_window_surface(&cfg, &surface_attribs)
+            .expect("Failed to create OpenGL surface")
+    };
+
+    let context = context
+        .make_current(&surface)
+        .expect("Failed to make OpenGL context current");
+
+    (event_loop, window, surface, context)
+}
+
+fn glow_context(context: &PossiblyCurrentContext) -> glow::Context {
+    unsafe {
+        glow::Context::from_loader_function_cstr(|s| context.display().get_proc_address(s).cast())
+    }
+}
+
+fn imgui_init(window: &Window) -> (WinitPlatform, imgui::Context) {
+    let mut imgui_context = imgui::Context::create();
+    imgui_context.set_ini_filename(None);
+
+    let mut winit_platform = WinitPlatform::new(&mut imgui_context);
+    winit_platform.attach_window(
+        imgui_context.io_mut(),
+        window,
+        imgui_winit_support::HiDpiMode::Rounded,
+    );
+
+    imgui_context.fonts().add_font(&[imgui::FontSource::DefaultFontData {
+        config: Some(imgui::FontConfig {
+            size_pixels: 24.0, // Increase font size from default 13.0  bvc
+            ..Default::default()
+        }),
+    }]);
+
+    let io = imgui_context.io_mut();
+    io.font_global_scale = (1.0 / winit_platform.hidpi_factor()) as f32;
+    io.config_flags |= imgui::ConfigFlags::DOCKING_ENABLE;
+
+    (winit_platform, imgui_context)
 }
